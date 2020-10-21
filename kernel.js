4ba5a1a3171bc28b2f52ffccabd86ecb0eab98c0
Paul Tarvydas <guitarvydas@gmail.com>
2020-10-21 07:42:40 -0400

    wip: begin rewrite of example1.html to conform to document

--- kernel.js
+++ kernel.js
@@ -1,139 +1,63 @@
-function Wire () {
-    var receivers = new ReceiversCollection (this);
-    var dataList = new DataQueue (this);
-    function installReceiver (part, inputPin, outputPin) { receivers.push ( {isNC=false, part, inputPin, outputPin} )};
-    function installNC (senderPart, senderOutputPin) { receivers.push ( {isNC= true, senderPart, null, senderPin} )};
-    function deliver () {
-	dataList.foreachDequeue ( 
-	    (d) => {
-		this.receivers.foreach(lock); // masking interrupts or turning off full-preemption would have the same effect
-		this.receivers.foreach( 
-		    (r) => {
-			if (r.isNC) {
-			    if (r.part.hasParent () ) {
-				// ignore event
-			    } else {
-			    // top-level has no parent - log event
-				console.log (r.part.name + " outputs " + data + " on pin " + r.outputPin);
-			    } else if (r.inputPin) {
-				r.part.enqueueInput (r.inputPin, data);
-			    } else {
-				r.part.enqueueOutput (r.outputPin, data);
-			    }
-			}
-		    })
-	    });
-	this.receivers.foreach(unlock);
+function Wire (wobject) {
+    this.sender = wobject.sender;
+    this.receivers = wobject.receivers;
+    this.isNC = wobject.isNC;
+    this.senderPin = wobject.senderPin;
+    this.lockReceivers = function () { // implement if using multi-tasking or bare-metal
+	// disable preemption
+	// this.receivers.forEach (lock);
+	// enable preemption
     };
-}
-
-function Dispatcher () {
-    var allParts = new PartsCollection ("_dispatcher_parts_");
-    function installPart (part) { this.allParts.add (part); };
-    function inject (part, pin, data) { // inject data event from the outside - need to remember to (re)start the Dispatcher
-	this.allParts.ensureContains (part);
-	part.enqueueInput ({pin, data});
+    this.unlockReceivers = function () { // implement if using multi-tasking or bare-metal
+	// disable preemption
+	// this.receivers.forEach (unlock);
+	// enable preemption
     };
-    function run () {
-	var keepLooping = true;
-	while (keepLooping) {
-	    keepLooping = false;
-	    this.allParts.foreach ( (part) => {
-		if ( part.isReady() ) {
-		    keepLooping = true;
-		    var e = part.dequeueInput ();
-		    part.react (e);
-		}
-	    } );
-	    this.distributeAllOutputEvents ();
-	}
-    }
-    function distributeAllOutputEvents () {
-	this.allParts.foreach ( (part) => this.distributeOutputs (part) );
-    }
-    function distributeOutputs (part) {
-	this.allParts.foreach ( 
-	    (part) => {
-		part.outputCollection.foreach(deliver);
-		part.outputCollection.reset ();
-	    }
-	)
-    }
-}
-
-dispatcher = new Dispatcher();
-
-function Schematic (parent, nameInParent) {
-    var parent = parent;
-    var name = nameInParent;
-    var inputQueue = new InputQueue (this);
-    var outputCollection = new OutputCollection (this);
-    var parts = new PartsCollection (this);
-    var isSchematic = true;
-
-    function isBusy () { return parts.every( (p) => { p.isNotBusy () }); };
-    function isNotBusy () { return ! this.isBusy (); };
-    function isReady () { return ( (0 < this.inputQueuelength) && this.isNotBusy ()); };
-    function installPart (p) { parts.push (p); };
-    function installWire (wire) {
-	wire.foreach ( 
-	    (w) => {
-		w.senders.foreach ( (part) => { this.parts.mustContain (part) } );
-		w.receivers.foreach ( (part) => { this.parts.mustContain (part) } );
-	    }
-	);
-	wires.add (wire);
-    };
-    function initialize () {
-	parts.foreach ( (part) => part.initialize ());
-	parts.foreach ( (part) => dispatcher.InstallPart (part) );
-    }
-}
-
-function Leaf (parent, nameInParent reactorFunction) {
-    var parent = parent;
-    var name = nameInParent;
-    var inputQueue = new InputQueue (this);
-    var outputCollection = new OutputCollection (this);
-    var isSchematic = false;
-    function isBusy () { return false; }
-    function isNotBusy () { return ! this.isBusy (); },
-    function isReady () { return ( (0 < this.inputQueueLength()) && this.isNotBusy ()); };
-    var reactor = reactorFunction;
-    function react (event) { this.reactor (event); }
-    function initialize () {};
-}
-
-function PartsCollection (owner) = {
-    var owner = owner;
-    var collection = [];
-    function ensureContains (part) {
-	if (collection.includes (part) {
-	} else{
-	    throw "Parts collection of " + owner.name " does not contain the part " + part.name;
+    this.deliver = function (data) {
+	if (this.isNC) {
+	    console.log("Part " + sender.name + " outputs " + data + " on pin " + senderPin);
+	} else {
+	    this.lockReceivers ();
+	    this.receivers.forEach(
+		function (r) {
+		    var pin = r.pin;
+		    r.part.inputQueue.push ( {pin, data} );
+		})
+	    this.unlockReceivers ();
 	}
     };
-    function add (part) {
-	this.collection.push (part);
-    };
-    function foreach (func) {
-	this.collection.foreach (func);
-    };
-}
-
-function InputQueue (owner) = {
-    var owner = owner;
-    var q = [];
-    function inputQueueLength () { return this.q.length; }
-    function dequeueInput () { return this.q.shift(); }
-    function enqueueInput (x) { this.q.push(x); }
-}
-    
-function OutputCollection (owner) = {
-    var owner = owner;
-    var collection = [];
-    function foreach (func) { collection.foreach (func); };
-    function reset () { collection = []; };
 };
 
-    function ReceiversCollections (owner) {
+function Kernel (top) {
+    this.topPart = top;
+    this.allParts = [];
+    this.findWire = function (schematic, senderPart, senderPin) {
+	var i;
+	for (i = 0; i < schematic.wires.length ; i += 1) {
+	    var sender = schematic.wires[i].sender;
+	    if (sender.part == senderPart && sender.pin == senderPin) {
+		return schematic.wires[i];
+	    }
+	}
+	throw "can't find wire for {" + part.name + ", " + outputPin + "}";
+    };
+    
+    this.send = function (part, outputEvent) {
+	var outputPin = outputEvent.pin; 
+	var outputData = outputEvent.data;
+	var parentSchematic = part.parent;
+	var outputWire = this.findWire (part.parent, part, outputPin);
+	outputWire.deliver (outputData);
+    };
+    
+    this.io = function () {
+	this.dispatch ();
+    };
+
+    this.dispatch = function () {
+	while (this.topPart.hasInputs()) {
+	    this.topPart.consumeOneEventIfReady();
+	}
+    };
+};
+
---
+++ text.js
@@ -0,0 +1,16 @@
+function TextComponent (id) {
+    this.parent = null;
+    this.id = id;
+    this.isSchematic = false;
+    this.inputQueue = [];
+    this.isReady = function () { return ( this.inputQueue.length > 0 ); };
+    this.hasInputs = function () {
+	return (0 < this.inputQueue.length);
+    };
+    this.consumeOneEventIfReady = function () {
+	if (this.isReady()) {
+	    var event = this.inputQueue.pop ();
+	    this.react (event);
+	}
+    };
+};
